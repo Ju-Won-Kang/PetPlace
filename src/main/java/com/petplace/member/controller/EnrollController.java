1041d54a1d270a618081c8f3862f36c548ac8343
Ju-Won-Kang <tlscjs401@gmail.com>
2024-10-04 18:20:47 +0900

    TMP

---
+++ src/main/java/com/petplace/member/controller/EnrollController.java
@@ -0,0 +1,30 @@
+package com.petplace.member.controller;/**
+ * packageName    : com.petplace.member.controller
+ * fileName       : EnrollController
+ * author         : jun
+ * date           : 2024. 10. 4.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024. 10. 4.        jun       최초 생성
+ */
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "enroll.me", value = "/enroll.me")
+public class EnrollController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
