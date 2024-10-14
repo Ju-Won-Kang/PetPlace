06d02a0f2c87db8a72f2baf71cf08e50e831e195
Anjaay <gnl1253@naver.com>
2024-10-14 09:34:39 +0900

    Feat : test.파일 삭제, detailController수정

--- src/main/java/com/petplace/shopping/controller/shoppingdetailServlet.java
+++ src/main/java/com/petplace/shopping/controller/shoppingdetailServlet.java
@@ -8,13 +8,11 @@ import com.petplace.shopping.model.dto.ShoppingDetailList;
 import com.petplace.shopping.service.ShoppingService;
 import com.petplace.shopping.service.ShoppingServiceImpl;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.websocket.Session;
 
 /**
  * Servlet implementation class shoppingdetailServlet
@@ -34,30 +32,29 @@ public class shoppingdetailServlet extends HttpServlet {
 		ArrayList<ShoppingDetailList> product = sService.selectDetailProduct(productNo);
 		System.out.println("Controller : " + product);
 		System.out.println(product.get(0).getFileLevel());
-		
 		//--------------------리뷰 컨트롤러--------------------------
-		int reviewCount = sService.selectReviewCount(productNo);	// 모든 리뷰 수 
-		
-		ArrayList<Review> list= sService.selectReviewList(productNo);	// 
-		
+		int reviewCount = sService.selectReviewCount(productNo);    // 모든 리뷰 수
+
+		ArrayList<Review> list= sService.selectReviewList(productNo);    //
+
 		System.out.println(list);
-		
-		
-		
-		// 리뷰가 없으면 리뷰가 없습니다 
+
+
+
+		// 리뷰가 없으면 리뷰가 없습니다
 		if(reviewCount < 0) {
 			request.setAttribute("errorMsg", "리뷰가 없습니다");
 		}else {
 			request.setAttribute("reviewCount", reviewCount); // 상품의 총 리뷰 개수
 		}
-		
 
-		
+
+
 		request.setAttribute("list", list);
+		
 		request.setAttribute("product", product);
 		request.getRequestDispatcher("views/shopping/shopping-detail.jsp").forward(request, response);
 	}
-	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
